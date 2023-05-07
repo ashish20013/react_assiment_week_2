b9d54f85f4e4823a868518e11f0739cd23c769e1
ashish20013 <ashishky892@gmail.com>
2023-05-07 22:33:38 +0530

    25may props card assiment

--- src/App.js
+++ src/App.js
@@ -1,65 +1,71 @@
 // import logo from './logo.svg';
 // import './App.css';
 import Card from './component/card';
-function App() {
-const allproduct =[
-  const cardOne {
-    id : 1,
-    Image : "https://tse4.mm.bing.net/th?id=OIP.MVar6WSmjZ-OOSt6vzIHHgHaHa&pid=Api&P=0 "
-    name : " watch"
-    discribtion : "  this watch is best look for your hand and please buy its"
-    price : "$60"
-  }
-  const cardTwo = {
-    id :2,
-    Image : "https://tse3.mm.bing.net/th?id=OIP.XEcHxtwFVjZiNXzPKs1uTwHaFj&pid=Api&P=0"
-    name : " watch"
-    discribtion : "  this watch is best look for your hand and please buy its"
-    price : "$60"
-  }
-  const cardThree = {
-    id :3,
-    Image : "https://tse1.mm.bing.net/th?id=OIP.wOCX8Gc3gPyLs7s-FzyrTwHaHa&pid=Api&P=0 "
-    name : " watch"
-    discribtion : "  this watch is best look for your hand and please buy its"
-    price : "$60"
-  }
-  const cardFour = {
-    id : 4,
-    Image : "https://tse1.mm.bing.net/th?id=OIP.2YaMVvrQJA3wcs9zggUzqAHaE7&pid=Api&P=0 "
-    name : " watch"
-    discribtion : "  this watch is best look for your hand and please buy its"
-    price : "$60"
-  }
-  const cardFive = {
-    id : 5,
-    Image : "https://tse4.mm.bing.net/th?id=OIP.yBIeMuxDuwO7jt1Gk0T__gHaLG&pid=Api&P=0 "
-    name : " watch"
-    discribtion : "  this watch is best look for your hand and please buy its"
-    price : "$60"
-  }
-]
-  //----------------------------------------
 
-  [cardOne , cardTwo , cardThree , cardfour , cardFive].map( (cardin) => < all product key = {all product.id} cardin = { cardin}/>)
-  return(
-  <div>
-      {allproduct}
-    </div>
-  )
+
+export default function App() {
+
+
+  const cardDetails=[ {
+      id : 1,
+      Pic: "https://pixabay.com/photos/cranes-industry-crane-system-port-7921948/",
+      Brand : " watch",
+      productName : "  this watch is best look for your hand and please buy its",
+      productDescription: " this watch is american tredition , very Smarty look ",
+      Price : "$60",
+      Rating : "****"
+    },{
+      id : 2,
+      Pic: "https://tse4.mm.bing.net/th?id=OIP.MVar6WSmjZ-OOSt6vzIHHgHaHa&pid=Api&P=0 ",
+      Brand : " watch",
+      productName : "  this watch is best look for your hand and please buy its",
+      productDescription: " this watch is american tredition , very Smarty look ",
+      Price : "$60",
+      Rating : "****"
+    },{
+      id : 3,
+      Pic: "https://tse4.mm.bing.net/th?id=OIP.MVar6WSmjZ-OOSt6vzIHHgHaHa&pid=Api&P=0 ",
+      Brand : " watch",
+      productName : "  this watch is best look for your hand and please buy its",
+      productDescription: " this watch is american tredition , very Smarty look ",
+      Price : "$60",
+      Rating : "****"
+    },{
+      id : 4,
+      Pic: "https://tse4.mm.bing.net/th?id=OIP.MVar6WSmjZ-OOSt6vzIHHgHaHa&pid=Api&P=0 ",
+      Brand : " watch",
+      productName : "  this watch is best look for your hand and please buy its",
+      productDescription: " this watch is american tredition , very Smarty look ",
+      Price : "$60",
+      Rating : "****"
+    },{
+      id : 5,
+      Pic: "https://tse4.mm.bing.net/th?id=OIP.MVar6WSmjZ-OOSt6vzIHHgHaHa&pid=Api&P=0 ",
+      Brand : " watch",
+      productName : "  this watch is best look for your hand and please buy its",
+      productDescription: " this watch is american tredition , very Smarty look ",
+      Price : "$60",
+      Rating : "****"
+    }
+  ] 
+  
+
   return (
-    
-    <div className="App">
-      <header className="App-header">
+    <>
+    <div>
+      {cardDetails.map((props)=>{
         
-        <Card  cardin ={cardOne}/>
-        <Card cardin = {cardTwo}/>
-        <Card cardin = {cardThree}/>
-        <Card cardin = {cardFour}/>
-        <Card cardin = {cardFive}/>
-      </header>
+        return (
+          <Card {...props}/>
+        )
+      })}
     </div>
-  );
+    </>
+    
+  )
 }
 
-export default App;
+
+
+
+// 25 may assiment
--- src/component/card.js
+++ src/component/card.js
@@ -1,23 +1,48 @@
-import { Fragment } from 'react';
-import './card.css'
+import React from 'react'
 
-export default function Card(cardin ) {
-    const { image , name , discribtion , price  } =  cardin
+export default function Card( props) {
 
-    return (
-        <Fragment>
-            <div className='flax'>
-                <div className = " product1"> 
-                <img className='d' src={image}/>
-                <h1 className='a'> {name} </h1>
-                <p className='b'> {discribtion}</p>
-                <h2 className='c'>{price}</h2>
-                <butoon className="d" onclick =" function (){alert(Like)}" >ADD TO CART</butoon>
-            </div>
+  const {Pic , Brand , productName , productDescription , Price , Rating} = props
 
-            </div>
-        </Fragment>
-
+const btn = ()=>{
+    return(
+         alert ( 'Product of is  {productName} , which is Shoes of brand Adidas has of rupees {Price} is added in the cart , please proceed to pay !!!')
     )
-
 }
+
+  return (
+    <>
+    <div>
+        <img src={Pic}/>
+        <h2>{productName}</h2>
+        <h4>{Brand}</h4>
+        <p>{productDescription}</p>
+        <h2><b>{Price}</b></h2>
+        <p>{Rating}</p>
+        <button onClick={btn}>Add to Card</button>
+
+    </div>
+    </>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+// productDetails = {
+//     productId : ‘1’,
+//     productPic : ‘’,
+//     productName : ‘’,
+//     Brand : ‘
+//     productDescription : ‘’,
+//     Price : ‘7$’,
+//     Rating : 
+//  }
+ 
